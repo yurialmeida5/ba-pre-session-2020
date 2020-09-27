224ec004d91fdd12845d7a875eef05aa7c31c1c1
Yuri Almeida Cunha <yurialmeida5@gmail.com>
2020-09-27 02:59:56 -0700

    Add Cunha_Yuri Exam

---
+++ exam/Cunha_Yuri/IT/my_script.sh
@@ -0,0 +1,5 @@
+mkdir ba-pre-session-2020/exam/Cunha_Yuri/IT/made_by_my_script
+echo "Hello world" > ba-pre-session-2020/exam/Cunha_Yuri/IT/made_by_my_script/hello.txt
+ls ba-pre-session-2020/exam/Cunha_Yuri/IT/made_by_my_script > ba-pre-session-2020/exam/Cunha_Yuri/IT/made_by_my_script/list_of_files.txt
+
+
